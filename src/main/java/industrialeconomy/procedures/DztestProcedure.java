f63ace414fb60619d6a098763f21d9f24f3beb80
desperis1 <dusan.zongor@gmail.com>
2022-01-22 09:07:00 +0100

    dztest_sync

---
+++ src/main/java/industrialeconomy/procedures/DztestProcedure.java
@@ -0,0 +1,41 @@
+package industrialeconomy.procedures;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.Map;
+
+import industrialeconomy.block.SandstoneextractorBlock;
+
+import industrialeconomy.IndustrialEconomyMod;
+
+public class DztestProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency world for procedure Dztest!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency x for procedure Dztest!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency y for procedure Dztest!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency z for procedure Dztest!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		world.setBlockState(new BlockPos((int) x, (int) y, (int) z), SandstoneextractorBlock.block.getDefaultState(), 3);
+	}
+}
