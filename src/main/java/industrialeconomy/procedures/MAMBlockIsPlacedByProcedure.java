c4fc38c46600185340aa3b1595a44152dc1a5aca
KeLsO29 <matus121164@gmail.com>
2022-01-23 09:51:42 +0100

    MAM

---
+++ src/main/java/industrialeconomy/procedures/MAMBlockIsPlacedByProcedure.java
@@ -0,0 +1,57 @@
+package industrialeconomy.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.BlockState;
+
+import java.util.Map;
+
+import industrialeconomy.IndustrialEconomyMod;
+
+public class MAMBlockIsPlacedByProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency world for procedure MAMBlockIsPlacedBy!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency x for procedure MAMBlockIsPlacedBy!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency y for procedure MAMBlockIsPlacedBy!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency z for procedure MAMBlockIsPlacedBy!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency entity for procedure MAMBlockIsPlacedBy!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		Entity entity = (Entity) dependencies.get("entity");
+		if (!world.isRemote()) {
+			BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+			TileEntity _tileEntity = world.getTileEntity(_bp);
+			BlockState _bs = world.getBlockState(_bp);
+			if (_tileEntity != null)
+				_tileEntity.getTileData().putString("owner", (entity.getDisplayName().getString()));
+			if (world instanceof World)
+				((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+		}
+	}
+}
---
+++ src/main/java/industrialeconomy/procedures/MAMUpdateTickProcedure.java
@@ -0,0 +1,96 @@
+package industrialeconomy.procedures;
+
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.block.BlockState;
+
+import java.util.Map;
+
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+
+import industrialeconomy.IndustrialEconomyMod;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.Gson;
+
+public class MAMUpdateTickProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency world for procedure MAMUpdateTick!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency x for procedure MAMUpdateTick!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency y for procedure MAMUpdateTick!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				IndustrialEconomyMod.LOGGER.warn("Failed to load dependency z for procedure MAMUpdateTick!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		File playerConfig = new File("");
+		com.google.gson.JsonObject mainObject = new com.google.gson.JsonObject();
+		playerConfig = (File) new File((FMLPaths.GAMEDIR.get().toString() + "/config/"), File.separator + ((new Object() {
+			public String getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getString(tag);
+				return "";
+			}
+		}.getValue(world, new BlockPos((int) x, (int) y, (int) z), "owner")) + ".json"));
+		{
+			try {
+				BufferedReader bufferedReader = new BufferedReader(new FileReader(playerConfig));
+				StringBuilder jsonstringbuilder = new StringBuilder();
+				String line;
+				while ((line = bufferedReader.readLine()) != null) {
+					jsonstringbuilder.append(line);
+				}
+				bufferedReader.close();
+				mainObject = new Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
+				if (!world.isRemote()) {
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					TileEntity _tileEntity = world.getTileEntity(_bp);
+					BlockState _bs = world.getBlockState(_bp);
+					if (_tileEntity != null)
+						_tileEntity.getTileData().putDouble("Energy", mainObject.get("Energy").getAsDouble());
+					if (world instanceof World)
+						((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			Gson mainGSONBuilderVariable = new GsonBuilder().setPrettyPrinting().create();
+			try {
+				FileWriter fileWriter = new FileWriter(playerConfig);
+				fileWriter.write(mainGSONBuilderVariable.toJson(mainObject));
+				fileWriter.close();
+			} catch (IOException exception) {
+				exception.printStackTrace();
+			}
+		}
+	}
+}
